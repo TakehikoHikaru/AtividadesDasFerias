fb8f0a95d22b93b12bf4fb3753b78af49578da53
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 18:53:31 -0300

    Questao41

---
+++ AtividadeFerias/src/testes/TesteFuncionario2.java
@@ -0,0 +1,45 @@
+package testes;
+
+import Util.Teclado;
+import orientacaoAObjetos.Funcionario;
+
+public class TesteFuncionario2 {
+
+	public static void main(String[] args) {
+			
+			Funcionario funcionario_1 = new Funcionario();
+			
+			funcionario_1.nomeFuncionario = Teclado.lerTexto("Digite o nome do funcionario: ");
+			
+			
+			int num;
+			while(1<2) {
+				num = Teclado.lerInt("\nVer status do funcionario(1)\nAlterar nome do funcionario(2)\nAlterar salario do funcionario(3)\nSair(4)");
+				
+				switch (num) {
+				
+				case 1:
+				funcionario_1.ConsultaDados();
+				break;
+				
+				case 2:
+				funcionario_1.nomeFuncionario = Teclado.lerTexto("Digite o nome do funcionario: ");	
+				break;
+			
+				case 3:
+				funcionario_1.salarioFuncionario = Teclado.lerDouble("Digite o novo salario: ");
+				break;
+				
+				case 4:
+				break;
+				
+				default:
+					System.out.println("Erro");
+				
+				}
+				if(num == 4) {
+					break;
+				}
+			}
+	}
+}
