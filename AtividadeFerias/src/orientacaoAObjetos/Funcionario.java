c039fa48510ff6216017bdf7400fed3b1373c19d
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 18:18:01 -0300

    Questao25

--- AtividadeFerias/src/orientacaoAObjetos/Funcionario.java
+++ AtividadeFerias/src/orientacaoAObjetos/Funcionario.java
@@ -4,4 +4,14 @@ public class Funcionario {
 
 	public String nomeFuncionario;
 	public double salarioFuncionario;
+
+	public void AumentoSalario(double valor) {
+		salarioFuncionario += valor;
+	}
+	
+	public void ConsultaDados() {
+		System.out.printf("Nome: %s \nSalario: %f\n",this.nomeFuncionario,this.salarioFuncionario);
+	}
+	
+
 }
