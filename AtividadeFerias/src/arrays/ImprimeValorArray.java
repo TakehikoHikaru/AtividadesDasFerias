1eb7a351d71c68f08ef40aec7e64db24201052a8
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 19:03:29 -0300

    Questao43

---
+++ AtividadeFerias/src/arrays/ImprimeValorArray.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Random;
+
+public class ImprimeValorArray {
+
+	static Random gerar = new Random();
+	
+	public static void ImprimeArray(int[] array) {
+		for(int i = 0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+		}
+	
+	public static void PreencheArray(int[] array) {
+		for(int i = 0;i<array.length;i++) {
+			array[i] = gerar.nextInt();
+		}
+		}
+	
+	
+	public static void main(String[] args) {
+		
+		int[] Num =  new int[20];
+		
+		PreencheArray(Num);
+		ImprimeArray(Num);
+		
+		
+	}
+
+}
