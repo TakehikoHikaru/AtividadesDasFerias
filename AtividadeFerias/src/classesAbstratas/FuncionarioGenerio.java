193bf89040bf403f71b908508886744c04273776
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 20:50:32 -0300

    Questao92

--- AtividadeFerias/src/classesAbstratas/FuncionarioGenerio.java
+++ AtividadeFerias/src/classesAbstratas/FuncionarioGenerio.java
@@ -2,4 +2,29 @@ package classesAbstratas;
 
 public class FuncionarioGenerio {
 
+	private String nome;
+	private double salario;
+	
+	
+	
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+	
+	
+	
 }
---
+++ AtividadeFerias/src/testes/TesteFuncionarioGenerico.java
@@ -0,0 +1,14 @@
+package testes;
+
+import classesAbstratas.FuncionarioGenerio;
+
+public class TesteFuncionarioGenerico {
+
+	public static void main(String[] args) {
+		
+		
+		FuncionarioGenerio fun01 = new FuncionarioGenerio();
+
+	}
+
+}
