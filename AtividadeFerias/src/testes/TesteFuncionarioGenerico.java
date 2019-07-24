5950c1abcad4657838669b0919ff64b4049ef9b0
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 20:53:10 -0300

    Questao95

--- AtividadeFerias/src/testes/TesteFuncionarioGenerico.java
+++ AtividadeFerias/src/testes/TesteFuncionarioGenerico.java
@@ -1,13 +1,14 @@
 package testes;
 
 import classesAbstratas.FuncionarioGenerio;
+import classesAbstratas.GerenteDeBanco;
 
 public class TesteFuncionarioGenerico {
 
 	public static void main(String[] args) {
 		
 		
-		FuncionarioGenerio fun01 = new FuncionarioGenerio();
+		FuncionarioGenerio fun01 = new GerenteDeBanco();
 
 	}
 
