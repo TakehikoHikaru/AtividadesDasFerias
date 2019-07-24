6664b59620f335321131e3a4c8224cf4d1b35bfc
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 20:38:29 -0300

    Questao83

---
+++ AtividadeFerias/src/testes/TesteContaAbstrata.java
@@ -0,0 +1,13 @@
+package testes;
+
+import polimorfismo.ContaGenerica;
+
+public class TesteContaAbstrata {
+
+	public static void main(String[] args) {
+		
+		ContaGenerica con1 = new ContaGenerica();
+
+	}
+
+}
