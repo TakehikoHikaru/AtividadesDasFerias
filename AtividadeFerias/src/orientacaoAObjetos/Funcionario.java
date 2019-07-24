4e0e52f1f37ae152e4bf1e8231c65298783dec43
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 19:30:12 -0300

    Questao60

--- AtividadeFerias/src/orientacaoAObjetos/Funcionario.java
+++ AtividadeFerias/src/orientacaoAObjetos/Funcionario.java
@@ -2,6 +2,8 @@ package orientacaoAObjetos;
 
 public class Funcionario {
 
+
+
 	private String nomeFuncionario;
 	private double salarioFuncionario = 1000;
 	private double valeRefeicao;
@@ -14,9 +16,32 @@ public class Funcionario {
 		System.out.printf("Nome: %s \nSalario: %f\nVale Refeicão: %f\n",this.nomeFuncionario,this.salarioFuncionario,this.valeRefeicao);
 	}
 	
+	
 	public void AjustaTaxaValeRefeicao(double taxa) {
 		valeRefeicao += ((valeRefeicao/100)*taxa);
 	}
 	
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
 
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+
+	public double getSalarioFuncionario() {
+		return salarioFuncionario;
+	}
+
+	public void setSalarioFuncionario(double salarioFuncionario) {
+		this.salarioFuncionario = salarioFuncionario;
+	}
+
+	public double getValeRefeicao() {
+		return valeRefeicao;
+	}
+
+	public void setValeRefeicao(double valeRefeicao) {
+		this.valeRefeicao = valeRefeicao;
+	}
 }
