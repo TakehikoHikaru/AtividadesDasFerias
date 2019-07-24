29d1e376e46fb368e50541f6e0c0db54b92edf5c
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 19:48:58 -0300

    Questao65

---
+++ AtividadeFerias/src/testes/TesteTiposFuncionarios.java
@@ -0,0 +1,17 @@
+package testes;
+
+import orientacaoAObjetos.Gerente;
+import orientacaoAObjetos.Secretaria;
+import orientacaoAObjetos.Telefonista;
+
+public class TesteTiposFuncionarios {
+
+	public static void main(String[] args) {
+		
+		Secretaria secre = new Secretaria();
+		Gerente gere = new Gerente();
+		Telefonista tele = new Telefonista();
+
+	}
+
+}
