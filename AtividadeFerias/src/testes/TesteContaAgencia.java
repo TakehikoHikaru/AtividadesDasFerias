ac78d302ca4968b8605feecea046d39eeb297d53
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 18:06:18 -0300

    Questao20

---
+++ AtividadeFerias/src/testes/TesteContaAgencia.java
@@ -0,0 +1,19 @@
+package testes;
+
+import orientacaoAObjetos.Agencia;
+import orientacaoAObjetos.Conta;
+
+public class TesteContaAgencia {
+
+	public static void main(String[] args) {
+		
+		Agencia age01 = new Agencia();
+		Conta con01 = new Conta();
+		
+		age01.CodAgencia = 100;
+		con01.agencia = age01;
+
+		System.out.println(con01.agencia.CodAgencia);
+	}
+
+}
