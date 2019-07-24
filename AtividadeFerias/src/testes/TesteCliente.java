68cd2f965d69ddcb32dcdb9d31ff1fb331ed1c67
TakehikoHikaru <dospassosguilherme@gmail.com>
2019-07-24 16:37:41 -0300

    Questao03

---
+++ AtividadeFerias/src/testes/TesteCliente.java
@@ -0,0 +1,15 @@
+package testes;
+
+import orientacaoAObjetos.Cliente;
+
+public class TesteCliente {
+
+	public static void main(String[] args) {
+		
+		
+		Cliente Cli01 =  new Cliente();
+		Cliente Cli02 =  new Cliente();
+
+	}
+
+}
